ed26336a904b4d84e4ad5c8be54f3183d718c60d
Mix and Jam <mixandjamyt@gmail.com>
2019-04-17 20:00:45 -0300

    Initial Commit

---
+++ Assets/Scripts/TargetScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetScript : MonoBehaviour
+{
+
+    WarpController warp;
+
+    void Start()
+    {
+        warp = FindObjectOfType<WarpController>();
+    }
+
+    private void OnBecameVisible()
+    {
+        if (!warp.screenTargets.Contains(transform))
+            warp.screenTargets.Add(transform);
+    }
+
+    private void OnBecameInvisible()
+    {
+        if(warp.screenTargets.Contains(transform))
+            warp.screenTargets.Remove(transform);
+    }
+}
---
+++ Assets/Scripts/WarpController.cs
@@ -0,0 +1,303 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+using Cinemachine;
+using System;
+using UnityEngine.Rendering.PostProcessing;
+
+public class WarpController : MonoBehaviour
+{
+    private MovementInput input;
+    private Animator anim;
+
+    public bool isLocked;
+
+    public CinemachineFreeLook cameraFreeLook;
+    private CinemachineImpulseSource impulse;
+    private PostProcessVolume postVolume;
+    private PostProcessProfile postProfile;
+
+    [Space]
+
+    public List<Transform> screenTargets = new List<Transform>();
+    public Transform target;
+    public float warpDuration = .5f;
+
+    [Space]
+
+    public Transform sword;
+    public Transform swordHand;
+    private Vector3 swordOrigRot;
+    private Vector3 swordOrigPos;
+    private MeshRenderer swordMesh;
+
+    [Space]
+    public Material glowMaterial;
+
+    [Space]
+
+    [Header("Particles")]
+    public ParticleSystem blueTrail;
+    public ParticleSystem whiteTrail;
+    public ParticleSystem swordParticle;
+
+    [Space]
+
+    [Header("Prefabs")]
+    public GameObject hitParticle;
+
+    [Space]
+
+    [Header("Canvas")]
+    public Image aim;
+    public Image lockAim;
+    public Vector2 uiOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+
+        input = GetComponent<MovementInput>();
+        anim = GetComponent<Animator>();
+        impulse = cameraFreeLook.GetComponent<CinemachineImpulseSource>();
+        postVolume = Camera.main.GetComponent<PostProcessVolume>();
+        postProfile = postVolume.profile;
+        swordOrigRot = sword.localEulerAngles;
+        swordOrigPos = sword.localPosition;
+        swordMesh = sword.GetComponentInChildren<MeshRenderer>();
+        swordMesh.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("Blend", input.Speed);
+        UserInterface();
+
+        if (!input.canMove)
+            return;
+
+        if (screenTargets.Count < 1)
+            return;
+
+        if (!isLocked)
+        {
+            target = screenTargets[targetIndex()];
+        }
+
+        if (Input.GetMouseButtonDown(1)) {
+            LockInterface(true);
+            isLocked = true;
+        }
+
+        if (Input.GetMouseButtonUp(1) && input.canMove)
+        {
+            LockInterface(false);
+            isLocked = false;
+        }
+
+        if (!isLocked)
+            return;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            input.RotateTowards(target);
+            input.canMove = false;
+            swordParticle.Play();
+            swordMesh.enabled = true;
+            anim.SetTrigger("slash");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.visible = false;
+        }
+    }
+
+    private void UserInterface()
+    {
+
+        aim.transform.position = Camera.main.WorldToScreenPoint(target.position + (Vector3)uiOffset);
+
+        if (!input.canMove)
+            return;
+
+        Color c = screenTargets.Count < 1 ? Color.clear : Color.white;
+        aim.color = c;
+    }
+
+    void LockInterface(bool state)
+    {
+        float size = state ? 1 : 2;
+        float fade = state ? 1 : 0;
+        lockAim.DOFade(fade, .15f);
+        lockAim.transform.DOScale(size, .15f).SetEase(Ease.OutBack);
+        lockAim.transform.DORotate(Vector3.forward * 180, .15f, RotateMode.FastBeyond360).From();
+        aim.transform.DORotate(Vector3.forward * 90, .15f, RotateMode.LocalAxisAdd);
+    }
+
+    public void Warp()
+    {
+        GameObject clone = Instantiate(gameObject, transform.position, transform.rotation);
+        Destroy(clone.GetComponent<WarpController>().sword.gameObject);
+        Destroy(clone.GetComponent<Animator>());
+        Destroy(clone.GetComponent<WarpController>());
+        Destroy(clone.GetComponent<MovementInput>());
+        Destroy(clone.GetComponent<CharacterController>());
+
+        SkinnedMeshRenderer[] skinMeshList = clone.GetComponentsInChildren<SkinnedMeshRenderer>();
+        foreach (SkinnedMeshRenderer smr in skinMeshList)
+        {
+            smr.material = glowMaterial;
+            smr.material.DOFloat(2, "_AlphaThreshold", 5f).OnComplete(()=>Destroy(clone));
+        }
+
+        ShowBody(false);
+        anim.speed = 0;
+
+        transform.DOMove(target.position, warpDuration).SetEase(Ease.InExpo).OnComplete(()=>FinishWarp());
+
+        sword.parent = null;
+        sword.DOMove(target.position, warpDuration/1.2f);
+        sword.DOLookAt(target.position, .2f, AxisConstraint.None);
+
+        //Particles
+        blueTrail.Play();
+        whiteTrail.Play();
+
+        //Lens Distortion
+        DOVirtual.Float(0, -80, .2f, DistortionAmount);
+        DOVirtual.Float(1, 2f, .2f, ScaleAmount);
+    }
+
+    void FinishWarp()
+    {
+        ShowBody(true);
+
+        sword.parent = swordHand;
+        sword.localPosition = swordOrigPos;
+        sword.localEulerAngles = swordOrigRot;
+
+        SkinnedMeshRenderer[] skinMeshList = GetComponentsInChildren<SkinnedMeshRenderer>();
+        foreach (SkinnedMeshRenderer smr in skinMeshList)
+        {
+            GlowAmount(30);
+            DOVirtual.Float(30, 0, .5f, GlowAmount);
+        }
+
+        Instantiate(hitParticle, sword.position, Quaternion.identity);
+
+        target.GetComponentInParent<Animator>().SetTrigger("hit");
+        target.parent.DOMove(target.position + transform.forward, .5f);
+
+        StartCoroutine(HideSword());
+        StartCoroutine(PlayAnimation());
+        StartCoroutine(StopParticles());
+
+        isLocked = false;
+        LockInterface(false);
+        aim.color = Color.clear;
+
+        //Shake
+        impulse.GenerateImpulse(Vector3.right);
+
+        //Lens Distortion
+        DOVirtual.Float(-80, 0, .2f, DistortionAmount);
+        DOVirtual.Float(2f, 1, .1f, ScaleAmount);
+    }
+
+    IEnumerator PlayAnimation()
+    {
+        yield return new WaitForSeconds(.2f);
+        anim.speed = 1;
+    }
+
+    IEnumerator StopParticles()
+    {
+        yield return new WaitForSeconds(.2f);
+        blueTrail.Stop();
+        whiteTrail.Stop();
+    }
+
+    IEnumerator HideSword()
+    {
+        yield return new WaitForSeconds(.8f);
+        swordParticle.Play();
+
+        GameObject swordClone = Instantiate(sword.gameObject, sword.position, sword.rotation);
+
+        swordMesh.enabled = false;
+
+        MeshRenderer swordMR = swordClone.GetComponentInChildren<MeshRenderer>();
+        Material[] materials = swordMR.materials;
+
+        for (int i = 0; i < materials.Length; i++)
+        {
+            Material m = glowMaterial;
+            materials[i] = m;
+        }
+
+        swordMR.materials = materials;
+
+        for (int i = 0; i < swordMR.materials.Length; i++)
+        {
+            swordMR.materials[i].DOFloat(1, "_AlphaThreshold", .3f).OnComplete(() => Destroy(swordClone));
+        }
+
+        input.canMove = true;
+    }
+
+
+    void ShowBody(bool state)
+    {
+        SkinnedMeshRenderer[] skinMeshList = GetComponentsInChildren<SkinnedMeshRenderer>();
+        foreach (SkinnedMeshRenderer smr in skinMeshList)
+        {
+            smr.enabled = state;
+        }
+    }
+
+    void GlowAmount(float x)
+    {
+        SkinnedMeshRenderer[] skinMeshList = GetComponentsInChildren<SkinnedMeshRenderer>();
+        foreach (SkinnedMeshRenderer smr in skinMeshList)
+        {
+            smr.material.SetVector("_FresnelAmount", new Vector4(x, x, x, x));
+        }
+    }
+
+    void DistortionAmount(float x)
+    {
+        postProfile.GetSetting<LensDistortion>().intensity.value = x;
+    }
+    void ScaleAmount(float x)
+    {
+        postProfile.GetSetting<LensDistortion>().scale.value = x;
+    }
+
+    public int targetIndex()
+    {
+        float[] distances = new float[screenTargets.Count];
+
+        for (int i = 0; i < screenTargets.Count; i++)
+        {
+            distances[i] = Vector2.Distance(Camera.main.WorldToScreenPoint(screenTargets[i].position), new Vector2(Screen.width / 2, Screen.height / 2));
+        }
+
+        float minDistance = Mathf.Min(distances);
+        int index = 0;
+
+        for (int i = 0; i < distances.Length; i++)
+        {
+            if (minDistance == distances[i])
+                index = i;
+        }
+
+        return index;
+
+    }
+
+}
